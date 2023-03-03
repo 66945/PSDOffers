f00d04a167d1f85e920a062e17e8957d24f9791b
anorskdev <anorskdev@gmail.com>
2023-03-03 09:37:02 -0700

    Added two locations.

--- PSDOffers/PSDOffers/Offer.swift
+++ PSDOffers/PSDOffers/Offer.swift
@@ -10,6 +10,8 @@ import MapKit
 
 struct Offer {
     static var offers: [Offer] = [
+        Offer(name: "Cups Coffee", details: ["10% off"], logoURL: "sample_logo", location: [CLLocationCoordinate2D(latitude: 40.57306523957938, longitude: -105.11530191349455)]),
+        Offer(name: "Blue Federal Credit Union", details: ["Free checking"], logoURL: "sample_logo", location: [CLLocationCoordinate2D(latitude: 40.53243070893257, longitude: -105.07849515554979)]),
         Offer(name: "Name 1", details: [], logoURL: "sample_logo", location: []),
         Offer(name: "Jakob",  details: [], logoURL: nil,           location: []),
         Offer(name: "Sample", details: [], logoURL: "sample_logo", location: []),
