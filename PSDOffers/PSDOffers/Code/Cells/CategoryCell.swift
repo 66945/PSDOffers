3e63e1200ca8b1dee8716a520b2c72f02a96a9d3
Akivah <akivahlaviolette@gmail.com>
2023-04-11 09:59:48 -0600

    Moved files around for better organization

---
+++ PSDOffers/PSDOffers/Code/Cells/CategoryCell.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryCell.swift
+//  PSDOffers
+//
+//  Created by Laviolette, Akivah - Student on 4/11/23.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
